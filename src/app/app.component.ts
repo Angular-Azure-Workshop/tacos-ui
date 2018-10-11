8e49cd32e3f355f39331028b96d5e0419fb8b356
Simona Cotin <simona.cotin@gmail.com>
2018-10-11 17:40:00 -0400

    Track page views with AppInsights

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, Event, NavigationEnd } from '@angular/router';
 import { UserService } from './security/user.service';
+import { AppInsights } from 'applicationinsights-js';
+
 @Component({
   selector: 'app-root',
   template: '<router-outlet></router-outlet>',
@@ -12,5 +14,19 @@ export class AppComponent implements OnInit {
     userService.checkUserState();
   }
 
-  ngOnInit() {}
+  ngOnInit() {
+    AppInsights.downloadAndSetup({
+      instrumentationKey: '2c847176-e1b4-4b63-b2c5-996753f5410b'
+    });
+
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        if (event.url === '/') {
+          AppInsights.trackPageView('home');
+        } else {
+          AppInsights.trackPageView(event.url.substring(1));
+        }
+      }
+    });
+  }
 }
